4d9baba834a929d8a49f2ab26b5e18e51d96cf65
AJ Foster <web@aj-foster.com>
2019-10-04 17:24:44 -0400

    mix new my_app --sup
    Elixir 1.9.1

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule MyApp.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {MyApp.Application, []}
     ]
   end
 
